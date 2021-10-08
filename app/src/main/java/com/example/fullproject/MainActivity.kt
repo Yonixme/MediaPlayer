771464add22004871f9591948c87915195b5a3b4
Yonix <zyed.star@gmail.com>
2021-10-08 20:16:31 +0300

    start

---
+++ app/src/main/java/com/example/fullproject/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.fullproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.fullproject.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding :ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
