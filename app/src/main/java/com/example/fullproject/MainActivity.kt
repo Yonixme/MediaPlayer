e020e863c27b0a8d1b7ac77f78a9403360bdc1ea
Yonix <zyed.star@gmail.com>
2021-11-03 22:03:38 +0200

    1.1

--- app/src/main/java/com/example/fullproject/MainActivity.kt
+++ app/src/main/java/com/example/fullproject/MainActivity.kt
@@ -4,6 +4,7 @@ import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.SeekBar
 import com.example.fullproject.databinding.ActivityMainBinding
@@ -13,6 +14,7 @@ import java.lang.Exception
 class MainActivity : AppCompatActivity() {
     lateinit var binding :ActivityMainBinding
     private var mp: MediaPlayer? = null
+    private var isPlay : Boolean = true
     private var currentSong1 = mutableListOf(R.raw.crush)
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,15 +33,15 @@ class MainActivity : AppCompatActivity() {
                 initialiseSeekBar()
             }
             mp?.start()
-//            binding.timeNow.setText(Now(mp!!.currentPosition))
-            //Log.d("MainActivity" , "Duration: ${mp!!.duration/1000} second")
             binding.timeAll.setText(Now(mp!!.duration))
+            if(isPlay == false) isPlay = true
         }
 
         binding.pause.setOnClickListener {
             if (mp !== null) {
                 mp?.pause()
             }
+            if(isPlay == true) isPlay = false
         }
         binding.stop.setOnClickListener{
             if(mp !== null){
@@ -52,13 +54,22 @@ class MainActivity : AppCompatActivity() {
         }
         binding.timeView.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-             if(fromUser){
-                 mp?.seekTo(progress)
-             }
+                if(fromUser){
+                    try{
+                        binding.timeView.progress = mp!!.currentPosition
+                        binding.timeNow.setText(Now(mp!!.currentPosition))
+
+                    }catch (e: Exception) {
+                        binding.timeView.progress = 0
+                    }
+                    mp?.seekTo(progress)
+                }
             }
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                if(isPlay) mp?.pause()
             }
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                if(isPlay) mp?.start()
             }
 
         })
@@ -68,7 +79,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.timeView.max = mp!!.duration
 
-        val handler = Handler()
+        val handler = Handler(Looper.getMainLooper())
         handler.postDelayed(object: Runnable{
             override fun run() {
                 try{
@@ -92,7 +103,10 @@ class MainActivity : AppCompatActivity() {
             minute = help/60
             help %= 60
         }
-        str = "${minute.toString()}:${help.toString()}"
+        if(help > 9)
+            str = "${minute.toString()}:${help.toString()}"
+        else str = "${minute.toString()}:0${help.toString()}"
+
         return str
     }
 }
